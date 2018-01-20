f715580dfe4416086b2749d850cffb57a316f451
z <zhuhaiwen@microduino.cc>
2018-01-20 15:09:33 +0800

    服务器url

--- app/src/main/java/com/z/xwclient/utils/NetUrl.java
+++ app/src/main/java/com/z/xwclient/utils/NetUrl.java
@@ -6,7 +6,8 @@ package com.z.xwclient.utils;
 public class NetUrl {
   
 	//http://192.168.79.215:8080/
-	public static final String SERVERURL="http://116.196.95.87/xwkhd";
+	//public static final String SERVERURL="http://116.196.95.87/xwkhd";
+	public static final String SERVERURL="http://116.196.95.87/html/xwkhd";
 	public static final String NEWSCENTERURL=SERVERURL+"/categories.json";
 	public static final String PHOTOS=SERVERURL+"/photos/photos_1.json";
 }
